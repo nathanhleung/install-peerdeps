40231bcb27219bace4cd480d702ab265c72a2181
nathanhleung <nathanhleung@users.noreply.github.com>
2017-11-01 17:17:02 -0400

    add tests for last few pulls

--- src/cli.test.js
+++ src/cli.test.js
@@ -1,7 +1,25 @@
 import { spawn } from 'child_process';
+// Doesn't work with Jest, for some reason
+// @todo
+// import program from './cli';
 
 function spawnCli(extraArgs) {
   return spawn('node', ['--require', 'babel-register', 'cli.js'].concat(extraArgs));
+}
+
+function getCliInstallCommand(extraArgs, cb) {
+  // Always do dry run, so the command is the last line
+  // outputted
+  const cli = spawnCli(extraArgs.concat('--dry-run'));
+  const lines = [];
+  cli.stdout.on('data', (data) => {
+    lines.push(data);
+  });
+  cli.on('close', () => {
+    // The command will be the last line outputted by the cli
+    // during a dry run
+    return cb(data[data.length - 1]);
+  });
 }
 
 it('errors when more than one package is provided', () => {
@@ -25,7 +43,28 @@ it('errors when the package name argument is formatted incorrectly', () => {
   });
 });
 
+it('only installs peerDependencies when `--only-peers` is specified', () => {
+  getCliInstallCommand(
+    ['eslint-config-airbnb', '--only-peers'],
+    (command) => {
+      expect(command).not.stringMatching(/\beslint-config-airbnb\b/)
+    }
+  )
+});
+
+it('adds an explicit `--no-save` when using `--silent` with NPM', () => {
+  getCliInstallCommand(
+    ['eslint-config-airbnb', '--silent'],
+    (command) => {
+      expect(command).stringMatching(/\b--no-save\b/);
+    }
+  );
+});
+
 // @todo - tests for the actual install process
 // see https://github.com/sindresorhus/has-yarn/blob/master/test.js for details
 // Perhaps abstract the functionality of getting the package name
 // into its own function, and test that
+
+// Also see commander tests
+// https://github.com/tj/commander.js/blob/master/test/test.arguments.js
